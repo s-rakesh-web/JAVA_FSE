9f6309df3ee397b354e606ac79de3ce236e78319
s-rakesh-web <rakesh.2006.4.21@gmail.com>
2026-06-21 07:30:23 +0530

    Singleton example project completed

---
+++ SINGLETON_PATTERN_EXAMPLE.java
@@ -0,0 +1,17 @@
+class logger{
+
+    private static logger instance=new logger();
+    private logger(){
+    }
+    public static logger getInstance(){
+        return instance;
+    }
+}
+class SINGLETON_PATTERN_EXAMPLE{
+    public static void main(String[] args){
+    logger l1=logger.getInstance();
+    logger l2=logger.getInstance();
+
+    System.out.println(l1==l2);
+    }
+}
