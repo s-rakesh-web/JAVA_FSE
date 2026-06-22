11cebb9a760853c4cf77227eaec62d2cdac516cb
s-rakesh-web <rakesh.2006.4.21@gmail.com>
2026-06-22 09:54:05 +0530

    Strategy Pattern completed

---
+++ StrategyPatternExample.java
@@ -0,0 +1,44 @@
+interface PaymentStrategy{
+    void pay(double amount);
+}
+
+class CreditCard implements PaymentStrategy{
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Credit Card Processing Payment:"+amount);
+    }
+}
+
+class PayPal implements PaymentStrategy{
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paypal Processing Payment:"+amount);
+    }
+}
+
+class PaymentContext{
+    PaymentStrategy paymentStrategy;
+    public PaymentContext(PaymentStrategy paymentStrategy){
+        this.paymentStrategy=paymentStrategy;
+    }
+
+    public void payment(double amount){
+        paymentStrategy.pay(amount);
+    }
+}
+
+
+
+public class StrategyPatternExample {
+    public static void main(String[] args){
+        PaymentContext context=new PaymentContext(new CreditCard());
+        context.payment(161617.0);
+
+        PaymentContext con=new PaymentContext(new PayPal());
+        con.payment(161617.0);
+
+
+    }
+}
